52cca0b1306929aa47fa812a856b83b472336fa8
Pakchan <1625787@donga.ac.kr>
2024-06-17 09:47:26 +0900

    프로젝트 생성

---
+++ src/main/java/org/example/metabox/MetaboxApplication.java
@@ -0,0 +1,13 @@
+package org.example.metabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/metabox/MetaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.metabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
