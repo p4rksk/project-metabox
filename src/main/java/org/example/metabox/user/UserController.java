d4af6f255786a85984a7deec42123a3a30a2f443
SCHcoding <codingb007@gmail.com>
2024-06-19 17:17:58 +0900

    Feat : kakaoOAuth responseDTO 작성

--- src/main/java/org/example/metabox/user/UserController.java
+++ src/main/java/org/example/metabox/user/UserController.java
@@ -5,12 +5,14 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
     private final HttpSession session;
+    private final UserService userService;
 
     @GetMapping("/")
     public String mainForm(HttpServletRequest request) {
@@ -40,6 +42,31 @@ public class UserController {
     @GetMapping("/mypage/detail-saw")
     public String mySawDetail(HttpServletRequest request) {
         return "user/mypage-detail-saw";
+    }
+
+//     kakao, naver 공통
+//    @GetMapping("/oauth/callback")
+//    public String oauthCallback(HttpServletRequest request) {
+//        String code = request.getParameter("code");
+//        String provider = request.getParameter("provider");
+//
+//        if ("kakao".equals(provider)) {
+////        User sessionUser = userService.loginKakao(code);
+//
+//        } else if ("naver".equals(provider)) {
+////            User sessionUser = userService.loginNaver(code);
+//        }
+////        session.setAttribute("sessionUser", sessionUser);
+//        return "redirect:/";
+//    }
+
+    // 일단 카카오만
+    @GetMapping("/oauth/callback/kakao")
+    public String oauthCallback(String code) {
+        System.out.println("코드 받나요 : " + code);
+//        User sessionUser = userService.loginKakao(code);
+//        session.setAttribute("sessionUser", sessionUser);
+        return "redirect:/";
     }
 
 }
--- src/main/java/org/example/metabox/user/UserResponse.java
+++ src/main/java/org/example/metabox/user/UserResponse.java
@@ -1,4 +1,43 @@
 package org.example.metabox.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 public class UserResponse {
+
+    @Data
+    public static class TokenDTO {
+        @JsonProperty("access_token")
+        private String accessToken;
+        @JsonProperty("token_type")
+        private String tokenType;
+        @JsonProperty("refresh_token")
+        private String refreshToken;
+        @JsonProperty("expires_in")
+        private Integer expiresIn;
+        private List<String> scope;
+        @JsonProperty("refresh_token_expires_in")
+        private Integer refreshTokenExpiresIn;
+    }
+
+    @Data
+    public static class KakaoUserDTO {
+        private Long id;
+        @JsonProperty("connected_at")
+        private Timestamp connectedAt;
+        private Properties properties;
+
+        @Data
+        class Properties {
+            private String nickname;
+            @JsonProperty("profile_image")
+            private String profileImage;
+
+        }
+    }
+
+
 }
