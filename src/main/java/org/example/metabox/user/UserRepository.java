846edfc0944458da729a836e12e4fa8a5b1b5c2b
SCHcoding <codingb007@gmail.com>
2024-06-20 22:37:00 +0900

    Comment : 주석

--- src/main/java/org/example/metabox/user/UserRepository.java
+++ src/main/java/org/example/metabox/user/UserRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    // OAuth 중복 닉네임 있는지 확인 쿼리
     @Query("select u from User u where u.nickname = :nickname")
     User findByNickname(String nickname);
 
+    // OAuth 회원탈톼 (pk로 해야하면 말해주세여)
     @Modifying
     @Query("delete from User u where u.nickname = :nickname")
     void deleteByNickname(String nickname);
