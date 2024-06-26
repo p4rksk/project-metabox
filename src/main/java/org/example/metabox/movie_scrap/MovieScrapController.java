151a98177b518e4a555b79037e3c5e643b972b36
lhyeonnit <lhyeonnit@gmail.com>
2024-06-26 12:22:44 +0900

    영화 스크랩 완

--- src/main/java/org/example/metabox/movie_scrap/MovieScrapController.java
+++ src/main/java/org/example/metabox/movie_scrap/MovieScrapController.java
@@ -1,9 +1,23 @@
 package org.example.metabox.movie_scrap;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.example.metabox.user.SessionUser;
+import org.example.metabox.user.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class MovieScrapController {
+    private final MovieScrapService movieScrapService;
+    private final HttpSession session;
+
+    @PostMapping("/scrap/{id}/movie")
+    public String scrapMovie(@PathVariable Integer id) {
+        SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
+        movieScrapService.movieDetailScrap(id,sessionUser.getId());
+        return "redirect:/movies/list";
+    }
 }
--- src/main/java/org/example/metabox/movie_scrap/MovieScrapResponse.java
+++ src/main/java/org/example/metabox/movie_scrap/MovieScrapResponse.java
@@ -1,4 +1,17 @@
 package org.example.metabox.movie_scrap;
 
+import lombok.Data;
+
 public class MovieScrapResponse {
+
+    @Data
+    public static class ScrapDTO{
+        private int userId;
+        private int movieId;
+
+        public ScrapDTO(MovieScrap movieScrap) {
+            this.userId = movieScrap.getUser().getId();
+            this.movieId = movieScrap.getMovie().getId();
+        }
+    }
 }
