d87146a42d0b0d582ba88bb545c8043a2bfbf337
lhyeonnit <lhyeonnit@gmail.com>
2024-06-27 17:51:19 +0900

    별점 하는중

--- src/main/java/org/example/metabox/review/ReviewRequest.java
+++ src/main/java/org/example/metabox/review/ReviewRequest.java
@@ -1,4 +1,27 @@
 package org.example.metabox.review;
 
+import lombok.Data;
+import org.example.metabox.movie.Movie;
+import org.example.metabox.theater.Theater;
+import org.example.metabox.theater_scrap.TheaterScrap;
+import org.example.metabox.user.User;
+
 public class ReviewRequest {
+
+    @Data
+    public static class ReviewSaveDTO {
+        private User user;
+        private Movie movie;
+        private double rating;
+        private String comment;
+
+        public Review toEntity (User user, Movie movie) {
+            return Review.builder()
+                    .comment(comment)
+                    .rating(rating)
+                    .user(user)
+                    .movie(movie)
+                    .build();
+        }
+    }
 }
--- src/main/java/org/example/metabox/review/ReviewResponse.java
+++ src/main/java/org/example/metabox/review/ReviewResponse.java
@@ -1,4 +1,20 @@
 package org.example.metabox.review;
 
+import lombok.Data;
+import org.example.metabox.user.User;
+
 public class ReviewResponse {
+
+    @Data
+    public static class ReviewDTO {
+        private String comment;
+        private String nickname;
+        private String imgFilename;
+
+        public ReviewDTO(Review review, User user){
+            this.comment = review.getComment();
+            this.nickname = user.getNickname();
+            this.imgFilename = user.getImgFilename();
+        }
+    }
 }
