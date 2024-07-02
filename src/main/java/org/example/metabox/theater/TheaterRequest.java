0aa411e4a1e2f14ec3e7df96f6ee46c20adc2027
SCHcoding <codingb007@gmail.com>
2024-07-02 19:28:03 +0900

    theater login aop

--- src/main/java/org/example/metabox/theater/TheaterRequest.java
+++ src/main/java/org/example/metabox/theater/TheaterRequest.java
@@ -1,5 +1,7 @@
 package org.example.metabox.theater;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -16,7 +18,12 @@ public class TheaterRequest {
 
     @Data
     public class LoginDTO {
+        @NotEmpty(message = "ID가 공백일 수 없습니다.")
+        @Size(min = 2, max = 20, message = "ID는 최소 2자 이상 최대 20자 이하여야 합니다.")
         private String loginId;
+
+        @NotEmpty(message = "비밀번호가 공백일 수 없습니다.")
+        @Size(min = 4, max = 20, message = "비밀번호는 최소 4자 이상 최대 20자 이하여야 합니다.")
         private String password;
     }
 
